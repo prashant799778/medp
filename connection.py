b4f8bf6ccdea646caa20bf6df1516adde6d71f4d
Nisha2807 <57443914+Nisha2807@users.noreply.github.com>
2020-01-29 17:44:00 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -5,7 +5,7 @@ def DBconnection():
     mysqlcon = pymysql.connect(host='localhost',
                             user='root',
                             password='OHAvgawzUw##32',
-                            db='FandomLive',
+                            db='medParliament',
                             charset='utf8mb4',
                             cursorclass=pymysql.cursors.DictCursor)
     
