a05609e6c621828ceff28b380e6b6add2e5ea857
Nisha2807 <57443914+Nisha2807@users.noreply.github.com>
2020-01-30 18:21:18 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -697,7 +697,7 @@ def UpdateUser():
                     output=databasefile.UpdateQuery("enterprenuerMaster",column,WhereCondition)
                 if (UserTypeId == 7):
                     WhereCondition = " and userId = '" + str(UserId) + "'"
-                    column=" address='" + str(address) + "',qualification  = '" + str(qualification) + "', batchOfQualification ='" + str(batchOfQualification) + "', institutionName ='" + str(institutionName) + "',universityName ='" + str(universityName) + "',universityAddress='" + str(universityAddress) + "',interestId ='" + str(interstId) + "'"  
+                    column=" address='" + str(address) + "',qualification  = '" + str(qualification) + "', batchOfQualification ='" + str(batchofQualification) + "', institutionName ='" + str(institutionName) + "',universityName ='" + str(universityName) + "',universityAddress='" + str(universityAddress) + "',interestId ='" + str(interstId) + "'"  
                     output=databasefile.UpdateQuery("studentMaster",column,WhereCondition)             
                 if data != "0":
                     column = 'userId,userName,userTypeId'
