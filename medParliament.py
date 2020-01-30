4b6c88d0232ee25e6f9efc4442126f1087ac4aeb
Nisha2807 <57443914+Nisha2807@users.noreply.github.com>
2020-01-30 16:23:38 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -680,18 +680,18 @@ def UpdateUser():
                 column = " email = '" + str(Email) + "',gender = '" + str(Gender) + "',country = '" + str(Country) + "', "
                 column = column +  " userName = '" + str(UserName) + "',city = '" + str(City) + "',mobileNo = '" + str(MobileNo) + "'"
                 data = databasefile.UpdateQuery("userMaster",column,WhereCondition)
-                    if (UserTypeId == 5):
-                        WhereCondition = " and userId = '" + str(UserId) + "'"
-                        column =" organization = '" + str(organization) + "',aboutProfile='" + str(aboutProfile) + "',designation='" + str(designation) + "'"
-                        output=databasefile.UpdateQuery("policyMakerMaster",column,WhereCondition)
-                    if (UserTypeId == 6):
-                        WhereCondition = " and userId = '" + str(UserId) + "'"
-                        column=" designation='" + str(designation) + "' , areaOfActivity ='" + str(areaOfActivity) + "',profileCategoryId='" + str(areaOfActivity) + "',interestId = '" + str(interestId) + "'"
-                        output=databasefile.UpdateQuery("enterprenuerMaster",column,WhereCondition)
-                    if (UserTypeId == 7):
-                        WhereCondition = " and userId = '" + str(UserId) + "'"
-                        column=" address='" + str(address) + "',qualification  = '" + str(qualifiaction) + "', batchOfQualification ='" + str(batchOfQualification) + "', institutionName ='" + str(institutionName) + "',universityName ='" + str(universityName) + "',universityAddress='" + str(universityAddress) + "',interestId ='" + str(interstId) + "'"  
-                        output=databasefile.UpdateQuery("studentMaster",column,WhereCondition)               
+                if (UserTypeId == 5):
+                    WhereCondition = " and userId = '" + str(UserId) + "'"
+                    column =" organization = '" + str(organization) + "',aboutProfile='" + str(aboutProfile) + "',designation='" + str(designation) + "'"
+                    output=databasefile.UpdateQuery("policyMakerMaster",column,WhereCondition)
+                if (UserTypeId == 6):
+                    WhereCondition = " and userId = '" + str(UserId) + "'"
+                    column=" designation='" + str(designation) + "' , areaOfActivity ='" + str(areaOfActivity) + "',profileCategoryId='" + str(areaOfActivity) + "',interestId = '" + str(interestId) + "'"
+                    output=databasefile.UpdateQuery("enterprenuerMaster",column,WhereCondition)
+                if (UserTypeId == 7):
+                    WhereCondition = " and userId = '" + str(UserId) + "'"
+                    column=" address='" + str(address) + "',qualification  = '" + str(qualifiaction) + "', batchOfQualification ='" + str(batchOfQualification) + "', institutionName ='" + str(institutionName) + "',universityName ='" + str(universityName) + "',universityAddress='" + str(universityAddress) + "',interestId ='" + str(interstId) + "'"  
+                    output=databasefile.UpdateQuery("studentMaster",column,WhereCondition)               
                 if data != "0":
                     column = 'userId,userName,userTypeId'
                     data = databasefile.SelectQuery("userMaster",column,WhereCondition,"",startlimit,endlimit)                  
