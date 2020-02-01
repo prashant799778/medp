e624d2a6ac7e853ceb94efd544dbf9c61428df4b
Nisha2807 <57443914+Nisha2807@users.noreply.github.com>
2020-02-01 18:36:55 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -1401,19 +1401,15 @@ def verifyPost():
                 WhereCondition = " and postId = '" + str(postId) + "'"
                 column = " status = '" + str("1") + "'"
                 data = databasefile.UpdateQuery("userPost",column,WhereCondition)
+                return data
             if statusid == '2':
                 WhereCondition = " and postId = '" + str(postId) + "'"
                 column = " status = '" + str("2") + "'"
                 data = databasefile.UpdateQuery("approvedBy",column,WhereCondition)
                 WhereCondition = " and postId = '" + str(postId) + "'"
                 column = " status = '" + str("2") + "'"
-                data = databasefile.UpdateQuery("userPost",column,WhereCondition)
-                if data != "0":
-                    column = 'approvedUserId,postId,userTypeId'
-                    WhereCondition="and postId='" + postId+ "'"
-                    data = databasefile.SelectQuery("approvedBy",column,WhereCondition,"",startlimit,endlimit)
-                    Data = {"status":"true","message":"","result":data["result"]}                  
-                    return Data
+                data = databasefile.UpdateQuery("userPost",column,WhereCondition)            
+                return Data
             else:
                 return commonfile.Errormessage()
         else:
