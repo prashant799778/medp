fd04ad76628ac0f41ee998ca7d30d7ab958cbf01
vijay.pal <vijay.pal@fourbrick.com>
2020-02-01 19:56:05 +0530

    side bar

--- med-parliame/src/app/user/user-policy/user-policy.component.ts
+++ med-parliame/src/app/user/user-policy/user-policy.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from 'src/app/services/user-service.service';
 import { AppSettings } from 'src/app/utils/constant';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-policy',
@@ -9,14 +10,32 @@ import { AppSettings } from 'src/app/utils/constant';
 })
 export class UserPolicyComponent implements OnInit {
 	UserPolicyList=[];
-	constructor(public userService: UserServiceService) { }
+	postStatus = [];
+	constructor(public userService: UserServiceService,
+				public router: Router) { }
 
 	ngOnInit() {
 		this.userService.getApiData(AppSettings.AllPolicyMaker).then(resp=>{
 			console.log(resp)
 			this.UserPolicyList = resp['result']
+			this.UserPolicyList.forEach(resp =>{
+				this.getStatus(resp.status)
+			})
 		
 		})
+	}
+	getStatus(status){
+
+		if(status == '0'){
+			this.postStatus.push(0)
+		}else if(status == '1'){
+			this.postStatus.push(1);
+		}else{
+			this.postStatus.push(2);
+		}
+	}
+	policyDetail(id,userTypeId){
+		this.router.navigate(['/policyDetails'],{queryParams: {id: id,userTypeId: userTypeId}})
 	}
 
 }
