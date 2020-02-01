0b9b7f3b8ac5b704e950c6aaf48911b5513a0298
Nisha2807 <57443914+Nisha2807@users.noreply.github.com>
2020-02-01 17:51:13 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -1642,7 +1642,7 @@ def userProfile():
                     return commonfile.Errormessage()
             if userTypeId == 6:
                 print('HELLO')
-                column="um.userName,um.email,cm.countryName,pcm.name"
+                column="um.userName,um.email,cm.countryName,pcm.name,"
                 column=column+"em.designation,"
                 column=column+" em.areaOfActivity"
                 WhereCondition=" and cm.id=um.countryId and pcm.id=em.profileCategoryId and um.UserId=em.userId and um.userId='" + str(userId) + "'"
@@ -1653,7 +1653,7 @@ def userProfile():
                 else:
                     return commonfile.Errormessage()
             if userTypeId == 7:
-                column="um.userName,um.email,cm.countryName,un.universityName,qm.qualificationName"
+                column="um.userName,um.email,cm.countryName,un.universityName,qm.qualificationName,"
                 column=column+"sm.address,"
                 column=column+" sm.batchOfQualification, sm.institutionName, sm.universityAddress"
                 WhereCondition=" and cm.id=um.countryId and un.id=sm.universityId and qm.id=sm.qualificationId and um.UserId=sm.userId and um.userId='" + str(userId) + "'"
