e04c50f95389b3b7baa9968e43481e294d5e165d
prashant <prashantgoyal.fourbrick@gmail.com>
2020-02-02 19:34:53 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -1647,7 +1647,7 @@ def userProfile():
                 column="*"
                 whereCondition="and ab.userId='" + userId+ "'"
                 data4 = databasefile.SelectQueryOrderby("userPost as ab",column,whereCondition,"",startlimit,endlimit,orderby)
-                data2={"userProfile":data1["result"],"userProfile":data4["result"]}
+                data2={"userProfile":data1["result"],"userPost":data4["result"]}
                 data3={"status":"true","message":"","result":data2}
                 if  data3:                     
                     return data3
@@ -1666,7 +1666,7 @@ def userProfile():
                 column="*"
                 whereCondition="and ab.userId='" + userId+ "'"
                 data4 = databasefile.SelectQueryOrderby("userPost as ab",column,whereCondition,"",startlimit,endlimit,orderby)
-                data2={"userProfile":data1["result"],"userProfile":data4["result"]}
+                data2={"userProfile":data1["result"],"userPost":data4["result"]}
                 data3={"status":"true","message":"","result":data2}
                 if  data3:                     
                     return data3
@@ -1684,7 +1684,7 @@ def userProfile():
                 column="*"
                 whereCondition="and ab.userId='" + userId+ "'"
                 data4 = databasefile.SelectQueryOrderby("userPost as ab",column,whereCondition,"",startlimit,endlimit,orderby)
-                data2={"userProfile":data1["result"],"userProfile":data4["result"]}
+                data2={"userProfile":data1["result"],"userPost":data4["result"]}
                 data3={"status":"true","message":"","result":data2}
                 if  data3:                     
                     return data3
