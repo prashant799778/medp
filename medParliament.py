f08ff53eb4b3cbb3699330262c772319e9cebbc9
prashant <prashantgoyal.fourbrick@gmail.com>
2020-02-05 11:51:53 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -1380,7 +1380,7 @@ def allPosts2():
             column="um.userName,um.email,um.countryId,um.city,pm.postDescription,pm.postId,pm.userId,pm.status,pm.id as Id,pm.postImage,pm.postTitle,pm.postImagePath,um.userTypeId as userTypeId,date_format(pm.dateCreate,'%Y-%m-%d %H:%i:%s')DateCreate"
             WhereCondition=" and um.userTypeId=pm.userTypeId and pm.userId=um.userId "
             data = databasefile.SelectQueryOrderby("userPost as pm,userMaster as um",column,WhereCondition,"",startlimit,endlimit,orderby)
-            print("11111111111111")
+            print("11111111111111",data)
 
           
 
