8189ecd6cf9f8d37c93638f1cdf4c315f2b710d0
prashant <prashantgoyal.fourbrick@gmail.com>
2020-02-07 13:59:45 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -3546,6 +3546,7 @@ def userDropdown():
        
 
         if data:
+            data.append({"id":0,"userName":"all"})
             #data["result"][0]["id"]=0           
             Data = {"status":"true","message":"","result":data["result"]}
             return Data
