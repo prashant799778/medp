0334069a541d8aa6c84289c17ba7c484a3c20529
sharad <sharad.fourbrick@gmail.com>
2020-02-09 19:48:04 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -3345,9 +3345,10 @@ def getNews():
                 if inputdata['id'] != "":
                     Id =inputdata["id"] 
                     WhereCondition=WhereCondition+" and id='"+str(Id)+"'"
+        orderby=" id "
         WhereCondition=WhereCondition+" and n.UserCreate=um.userId "
         column = "n.id,n.Status,n.newsTitle,n.userTypeId,n.summary,n.newsDesc, date_format(n.DateCreate,'%Y-%m-%d %H:%i:%s')DateCreate, concat('"+ ConstantData.GetBaseURL() + "',n.imagePath)imagePath ,um.userName "
-        data = databasefile.SelectQuery("news n,userMaster um",column,WhereCondition,"",startlimit,endlimit)
+        data = databasefile.SelectQueryOrderby("news n,userMaster um",column,WhereCondition,"",startlimit,endlimit,orderby)
         if data != "0":
             return data
         else:
