e05c13e2fa2ce7fc154746496ed6bf8c9d178277
sharad <sharad.fourbrick@gmail.com>
2020-02-11 16:08:24 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -2425,7 +2425,7 @@ def myPosts():
             userTypeId=inputdata["userTypeId"]
             userId=inputdata["userId"]
             #status=int(inputdata["status"])
-            column="pm.postDescription,pm.postId,pm.userId,pm.status,pm.id as Id,pm.postImage,pm.postTitle,pm.postImagePath,pm.userTypeId as userTypeId,convert_tz(date_format(pm.dateCreate,'%Y-%m-%d %H:%i:%s')DateCreate,'+00:00','-05:30')"
+            column="pm.postDescription,pm.postId,pm.userId,pm.status,pm.id as Id,pm.postImage,pm.postTitle,pm.postImagePath,pm.userTypeId as userTypeId,date_format(pm.dateCreate,'%Y-%m-%d %H:%i:%s')DateCreate"
             WhereCondition= " and pm.userId='" + str(userId) + "'and pm.userTypeId='" + str(userTypeId) + "'"
             data = databasefile.SelectQueryOrderby("userPost as pm",column,WhereCondition,"",startlimit,endlimit,orderby)
             print(data)
