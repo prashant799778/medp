59a184485415a7a00b67f755e50a404ec6dfd0a5
raj <raj.fourbrick@gmail.com>
2020-02-13 01:32:57 +0530

    cmt

---
+++ app/src/main/java/com/example/medparliament/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.medparliament;
+
+import android.app.Application;
+
+
+public class MyApplication extends Application {
+
+    public  MyApplication(){
+         super();
+
+
+
+    }
+}
