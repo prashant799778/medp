a3c8f480a1b5478addd0c5e2b6c45ed1d62f7dd8
sharad <sharad.fourbrick@gmail.com>
2020-02-20 14:01:12 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -41,7 +41,7 @@ def userId():
         print(output,"11111111111111111111")
         if output['result']!= "":
             
-            if output['result'][0]["emailVerificationStatus"]==0
+            if output['result'][0]["emailVerificationStatus"]==0:
                 y=output['result'][0]
                 if (y['userTypeId'] ==7)  or (y['userTypeId'] =='7') :
                     column=' emailVerificationStatus=1,status=0 '
