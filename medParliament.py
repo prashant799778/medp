e30f1a5780a89dbf2a31362ba8d4403b26985b1a
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-05 18:26:57 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -4944,7 +4944,7 @@ def landingPageDashboard1():
                     marketingInsightId=i['id']
                     whereCondition="and lki.marketingInsightId='"+str(marketingInsightId)+"'"
                     columns="count(*) as count"
-                    likeCount=databasefile.SelectQuery('likeMarketingInsight as lki',columns,whereCondition,"","")
+                    likeCount=databasefile.SelectQuery('likeMarketingInsight as lki',columns,whereCondition,"","","")
                     print(likeCount)
                     if likeCount['status']!='false':
                         lki=likeCount['result'][0]['count']
