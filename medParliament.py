57462958b9fbcfed7d7a86b03ecbd22c64e0eb8f
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-06 13:01:22 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -4960,7 +4960,24 @@ def landingPageDashboard1():
                 column7 = "mi.id,mi.Status,mi.UserCreate,mi.newsTitle,mi.userTypeId,mi.summary,mi.newsDesc,date_format(mi.DateCreate,'%Y-%m-%d %H:%i:%s')DateCreate, concat('"+ ConstantData.GetBaseURL() + "',mi.imagePath)imagePath,mi.length,mi.level,mi.language,mi.effort,mi.price,mi.videoTranscript"
                 data7 = databasefile.SelectQueryOrderby("upSkillsOpportunity  as mi",column7,WhereCondition,"","0","10",orderby)
                 if data7["result"]=="":
-                    data7["result"]=[]    
+                    data7["result"]=[]
+
+        if "userTypeId" not  in inputdata:
+           
+   
+            WhereCondition="and  mi.Status <2 and  mi.userTypeId=0"
+
+            column6 = "mi.id,mi.Status,mi.UserCreate,mi.newsTitle,mi.userTypeId,mi.summary,mi.newsDesc,date_format(mi.DateCreate,'%Y-%m-%d %H:%i:%s')DateCreate, concat('"+ ConstantData.GetBaseURL() + "',mi.imagePath)imagePath   "
+            data6 = databasefile.SelectQueryOrderby("marketingInsights as mi ",column6,WhereCondition,"","0","10",orderby)
+            if data6["result"]=="":
+                data6["result"]=[]
+           
+            column7 = "mi.id,mi.Status,mi.UserCreate,mi.newsTitle,mi.userTypeId,mi.summary,mi.newsDesc,date_format(mi.DateCreate,'%Y-%m-%d %H:%i:%s')DateCreate, concat('"+ ConstantData.GetBaseURL() + "',mi.imagePath)imagePath,mi.length,mi.level,mi.language,mi.effort,mi.price,mi.videoTranscript"
+            data7 = databasefile.SelectQueryOrderby("upSkillsOpportunity  as mi",column7,WhereCondition,"","0","10",orderby)
+            if data7["result"]=="":
+                data7["result"]=[]
+
+                        
 
 
 
