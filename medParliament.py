07ec9e35e7a79d526c38204dbfcfa2834a628cbc
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-11 12:40:16 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -4951,7 +4951,7 @@ def landingPageDashboard1():
         if "userTypeId" not in inputdata:
             WhereCondition229=" and ev.Status<2 and  ev.userTypeId=0 "
             column3 = "ev.id,ev.Status,ev.UserCreate,ev.eventTitle,ev.eventSummary,ev.eventLocation,date_format(ev.eventDate,'%Y-%m-%d %H:%i:%s')eventDate, date_format(ev.DateCreate,'%Y-%m-%d %H:%i:%s')DateCreate, concat('"+ ConstantData.GetBaseURL() + "',imagePath)imagePath   "
-            data3 = databasefile.SelectQueryOrderbyNew("parliamentEvent ev ",column3,WhereCondition229,"",0,0,orderby)
+            data3 = databasefile.SelectQueryOrderby("parliamentEvent ev ",column3,WhereCondition229,"",0,0,orderby)
             
             if data3["result"]=="":
                 data3["result"]=[]
@@ -4961,7 +4961,7 @@ def landingPageDashboard1():
         
             WhereCondition229=" and ev.Status<2 and  ev.userTypeId=0  or ev.userTypeId='"+str(userTypeId)+"'"
             column3 = "ev.id,ev.Status,ev.UserCreate,ev.eventTitle,ev.eventSummary,ev.eventLocation,date_format(ev.eventDate,'%Y-%m-%d %H:%i:%s')eventDate, date_format(ev.DateCreate,'%Y-%m-%d %H:%i:%s')DateCreate, concat('"+ ConstantData.GetBaseURL() + "',imagePath)imagePath   "
-            data3 = databasefile.SelectQueryOrderbyNew("parliamentEvent ev ",column3,WhereCondition229,"",0,0,orderby)
+            data3 = databasefile.SelectQueryOrderby("parliamentEvent ev ",column3,WhereCondition229,"",0,0,orderby)
             
             if data3["result"]=="":
                 data3["result"]=[]
@@ -5355,7 +5355,7 @@ def landingPageDashboardtest():
                 print("1111----")
                 WhereCondition229=" and ev.Status<2 and  ev.userTypeId=0  or ev.userTypeId='"+str(userTypeId)+"'"
                 column3 = "ev.id,ev.Status,ev.UserCreate,ev.eventTitle,ev.eventSummary,ev.eventLocation,date_format(ev.eventDate,'%Y-%m-%d %H:%i:%s')eventDate, date_format(ev.DateCreate,'%Y-%m-%d %H:%i:%s')DateCreate, concat('"+ ConstantData.GetBaseURL() + "',imagePath)imagePath   "
-                data3 = databasefile.SelectQueryOrderbyNew("parliamentEvent ev ",column3,WhereCondition229,"",0,0,orderby)
+                data3 = databasefile.SelectQueryOrderby("parliamentEvent ev ",column3,WhereCondition229,"",0,0,orderby)
                 
                 if data3["result"]=="":
                     data3["result"]=[]
