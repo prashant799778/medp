f5a79dd497dbd05084369a8381212c4a36322f99
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-11 13:45:48 +0530

    Revert "Update medParliament.py"
    This reverts commit 6c280da6c6caa5bad095f5b46d70d6fe405a5ec0.

--- medParliament.py
+++ medParliament.py
@@ -7277,7 +7277,6 @@ def userContent():
                     if inputdata['id'] != "":
                         Id =inputdata["id"]
                         inputdata1 = request.form.get('postImage')
-                        print(inputdata1)
                         print("inputdata=================",type(inputdata1))
                         if ((inputdata1!=ConstantData.GetBaseURL()) and (inputdata1 !="")) :
                             if  inputdata1 !=None: 
