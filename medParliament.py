92d5b68acd747326c853aa74f852d9615dd247f6
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-11 18:10:33 +0530

    test

--- medParliament.py
+++ medParliament.py
@@ -464,8 +464,7 @@ def getMailBody(userName, link):
          style="max-width: 600px;"> <tr> <td bgcolor="#ffffff" align="center" valign="top" \
          style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; \
          font-family: \'Roboto\', sans-serif; font-size: 48px; font-weight: 400; letter-spacing:\
-          4px; line-height: 48px;"> <h1 style="font-size: 48px; font-weight: 400; margin: 2;">\
-           MedParliament </h1> <img src="http://134.209.153.34:4200/assets/images/logo-sgn.svg"\
+          4px; line-height: 48px;"> <img src="http://134.209.153.34:4200/assets/images/logo-sgn.svg"\
             width="125" height="120" style="display: block; border: 0px;"/> </td></tr></table>\
              </td></tr><tr> <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">\
               <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">\
@@ -480,8 +479,7 @@ def getMailBody(userName, link):
                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:\
                     600px;"> <tr> <td bgcolor="#f4f4f4" align="left" style="padding: 0px 30px 30px 30px;\
                      color: #666666; font-family: \'Roboto\', sans-serif; font-size: 14px; font-weight:\
-                      400; line-height: 18px;"> <br><p style="margin: 0; text-align: center;">MedAchievers\
-                      MedParliament </p></td></tr></table> </td></tr></table></body>'
+                      400; line-height: 18px;"></td></tr></table> </td></tr></table></body>'
 
 @app.route('/SignUp', methods=['POST'])
 def SignUp1():
