168cced564b3b31c54b9aef6f0cf6647c3d7eb5a
sharad <sharad.fourbrick@gmail.com>
2020-03-12 01:00:59 +0530

    Update emailActivation.py

--- emailActivation.py
+++ emailActivation.py
@@ -6,13 +6,8 @@ def getactivationmail():#userName, link
      cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">\
       <tr> <td align="center" valign="top" style="padding: 40px 10px 40px 10px;">\
        </td></tr></table> </td></tr><tr> <td bgcolor="#e1a23c" align="center" style="padding:\
-        0px 10px 0px 10px;"> <table border="0" cellpadding="0" cellspacing="0" width="100%"\
-         style="max-width: 600px;"> <tr> <td bgcolor="#ffffff" align="center" valign="top" \
-         style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; \
-         font-family: \'Roboto\', sans-serif; font-size: 48px; font-weight: 400; letter-spacing:\
-          4px; line-height: 48px;"> <strong> style="font-size: 48px; font-weight: 400; margin: 2;">Welcome to MedParliament\
-                             <strong> </td></tr></table>\
-             </td></tr><tr> <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">\
+        0px 10px 0px 10px;"> <h1>Welcome to MedParliament</h1>\
+                          </td></tr><tr> <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">\
               <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">\
                <div style="min-height: 250px; text-align: center;background: #ffffff;"> \
                <p style="margin: 0">Hello Mr. ' + str("sharad") + '<br>Congratulations! Your account has been activated successfully </strong> <br> <br>\
