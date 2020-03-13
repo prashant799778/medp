110892dd9819866d447bf042367d688ec1eff7e9
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-13 18:44:22 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -3405,66 +3405,6 @@ def verifyPost12():
         return output
 
 
-
-@app.route('/eventInterest', methods=['POST'])
-def verifyPost123():
-    try:
-        print("nnnnnnnnnnnn",request.get_data(),"===================",type(request.get_data()))
-        inputdata =  commonfile.DecodeInputdata(request.get_data()) 
-        print("mmmmmmmmmmm")
-        startlimit,endlimit="",""
-        print("111111111111111111111111")
-        keyarr = ['userId','eventId','userTypeId']
-        commonfile.writeLog("eventInterest",inputdata,0)
-        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
-        print("22222222222222222222222")
-        if msg == "1":
-            approvedUserId = inputdata["userId"]
-            postId = inputdata["eventId"]
-            userTypeId = int(inputdata["userTypeId"])
-
-            WhereCondition = " and eventId = '" + str(postId) + "' and userId = '" + str(approvedUserId) + "'"
-            count = databasefile.SelectCountQuery("eventInterest",WhereCondition,"")
-            
-            if int(count) > 0:
-                print('F')         
-                return commonfile.EventInterstAlreadyExistMsg()
-            else:
-                print("333333333333333333333")
-             
-               
-                column = "userId,eventId,userTypeId,UserCreate"                
-                values = " '" + str(approvedUserId) + "','" + str(postId) + "','" + str(userTypeId) + "','" + str(approvedUserId) + "'"
-                data = databasefile.InsertQuery("eventInterest",column,values)
-                if data!="0":
-                    column="*"
-                    whereCondition=" and eventId ='" + str(postId) + "'"
-                    data1=databasefile.SelectQuery("eventInterest",column,whereCondition,"",startlimit,endlimit)
-                    if (data1["status"]!="false"):
-                        y=data1["result"][0]
-                        for i in data1['result']:
-                            i['likeStatus']=1
-                        y2=i['likeStatus']
-
-                      
-                       
-                        data1={"status":"true","result":y2,"message":""}
-                        return data1
-                    else:
-                        data1={"status":"true","result":"","message":"No Data Found"}
-                        return data1
-
-                else:
-                    return commonfile.Errormessage()
-        else:
-            return msg 
-
-    except Exception as e :
-        print("Exception---->" +str(e))           
-        output = {"status":"false","message":"something went wrong","result":""}
-        return output
-
-
 @app.route('/verifyPost', methods=['POST'])
 def verifyPost():
     try:
@@ -4878,7 +4818,181 @@ def likeMarketingInsight():
     except Exception as e :
         print("Exception---->" +str(e))           
         output = {"status":"false","message":"something went wrong","result":""}
-        return output        
+        return output  
+
+
+
+
+@app.route('/eventInterest', methods=['POST'])
+def verifyPost123():
+    try:
+        print("nnnnnnnnnnnn",request.get_data(),"===================",type(request.get_data()))
+        inputdata =  commonfile.DecodeInputdata(request.get_data()) 
+        print("mmmmmmmmmmm")
+        startlimit,endlimit="",""
+        print("111111111111111111111111")
+        keyarr = ['userId','eventId','userTypeId']
+        commonfile.writeLog("eventInterest",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        print("22222222222222222222222")
+        if msg == "1":
+            approvedUserId = inputdata["userId"]
+            postId = inputdata["eventId"]
+            userTypeId = int(inputdata["userTypeId"])
+
+            WhereCondition = " and eventId = '" + str(postId) + "' and userId = '" + str(approvedUserId) + "'"
+            count = databasefile.SelectCountQuery("eventInterest",WhereCondition,"")
+            
+            if int(count) > 0:
+                print('F')         
+                return commonfile.EventInterstAlreadyExistMsg()
+            else:
+                print("333333333333333333333")
+             
+               
+                column = "userId,eventId,userTypeId,UserCreate"                
+                values = " '" + str(approvedUserId) + "','" + str(postId) + "','" + str(userTypeId) + "','" + str(approvedUserId) + "'"
+                data = databasefile.InsertQuery("eventInterest",column,values)
+                if data!="0":
+                    column="*"
+                    whereCondition=" and eventId ='" + str(postId) + "'"
+                    data1=databasefile.SelectQuery("eventInterest",column,whereCondition,"",startlimit,endlimit)
+                    if (data1["status"]!="false"):
+                        y=data1["result"][0]
+                        for i in data1['result']:
+                            i['likeStatus']=1
+                        y2=i['likeStatus']
+
+                      
+                       
+                        data1={"status":"true","result":y2,"message":""}
+                        return data1
+                    else:
+                        data1={"status":"true","result":"","message":"No Data Found"}
+                        return data1
+
+                else:
+                    return commonfile.Errormessage()
+        else:
+            return msg 
+
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"status":"false","message":"something went wrong","result":""}
+        return output
+
+
+@app.route('/eventInterest1', methods=['POST'])
+def verifyPost1231():
+    try:
+        print("nnnnnnnnnnnn",request.get_data(),"===================",type(request.get_data()))
+        inputdata =  commonfile.DecodeInputdata(request.get_data()) 
+        print("mmmmmmmmmmm")
+        startlimit,endlimit="",""
+        print("111111111111111111111111")
+        keyarr = ['userId','eventId','userTypeId']
+        commonfile.writeLog("eventInterest",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        print("22222222222222222222222")
+        if msg == "1":
+            approvedUserId = inputdata["userId"]
+            postId = inputdata["eventId"]
+            userTypeId = int(inputdata["userTypeId"])
+
+            WhereCondition = " and eventId = '" + str(postId) + "' and userId = '" + str(approvedUserId) + "'"
+            count = databasefile.SelectCountQuery("eventInterest",WhereCondition,"")
+            
+            if int(count) > 0:
+                print('F')         
+                return commonfile.EventInterstAlreadyExistMsg()
+            else:
+                print("333333333333333333333")
+             
+               
+                column = "userId,eventId,userTypeId,UserCreate"                
+                values = " '" + str(approvedUserId) + "','" + str(postId) + "','" + str(userTypeId) + "','" + str(approvedUserId) + "'"
+                data = databasefile.InsertQuery("eventInterest",column,values)
+                if data!="0":
+                    column="count(*) as count"
+                    whereCondition="  and eventId ='" + str(postId) + " "
+                    data1=databasefile.SelectQuery("eventInterest",column,whereCondition,"",startlimit,endlimit)
+                    if (data1["status"]!="false"):
+                        o=[]
+                        y=data1["result"][0]
+                        print(y,"+++++++++++++++++=")
+                        whereCondition99= " and eventId ='" + str(postId) + "' and userId='" + str(approvedUserId) + "'"
+                        column88="status"
+                        da1=databasefile.SelectQuery("eventInterest",column88,whereCondition99,"",startlimit,endlimit)
+                        if da1['status'] != 'false':
+                            for i in da1['result']:
+                                i['makedone']=1
+                                y2={"makedone":1}
+                                y.update(y2)
+                                o.append(y)        
+                       
+
+                      
+                       
+                        data1={"status":"true","result":o,"message":""}
+                        return data1
+                    else:
+                        data1={"status":"true","result":"","message":"No Data Found"}
+                        return data1
+
+                else:
+                    return commonfile.Errormessage()
+        else:
+            return msg 
+
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"status":"false","message":"something went wrong","result":""}
+        return output
+
+
+
+@app.route('/commentsEvent', methods=['POST'])
+def commentsevent():
+    try:
+        print("nnnnnnnnnnnn",request.get_data(),"===================",type(request.get_data()))
+        inputdata =  commonfile.DecodeInputdata(request.get_data()) 
+        print("mmmmmmmmmmm")
+        startlimit,endlimit="",""
+        print("111111111111111111111111")
+        keyarr = ['userId','eventId','userTypeId','commentDescription']
+        commonfile.writeLog("verifyPost",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        print("22222222222222222222222")
+        if msg == "1":
+            userId = inputdata["userId"]
+            postId = inputdata["eventId"]
+            userTypeId = int(inputdata["userTypeId"])
+            print("333333333333333333333")
+            if (userTypeId == 7) or (userTypeId =='7'):
+         
+                commentDescription=inputdata['commentDescription']
+                column = "userId,eventId,userTypeId,commentDescription,status"                
+                values = " '" + str(userId) + "','" + str(postId) + "','" + str(userTypeId) + "','" + str(commentDescription) + "','" + str('1')  + "'"
+                data = databasefile.InsertQuery("eventComment",column,values)
+            else:
+                commentDescription=inputdata['commentDescription']
+                column = "userId,eventId,userTypeId,commentDescription,status"                
+                values = " '" + str(userId) + "','" + str(postId) + "','" + str(userTypeId) + "','" + str(commentDescription)+ "','" + str('0') + "'"
+                data = databasefile.InsertQuery("eventComment",column,values)
+
+            if data!="0":
+                return data
+            else:
+                return commonfile.Errormessage()
+        else:
+            return msg 
+
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"status":"false","message":"something went wrong","result":""}
+        return output
+                
+
 
 
 
