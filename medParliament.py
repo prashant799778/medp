c248bf1849fe4b87f3dc0ad7d0d0a19b8e7e913d
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-17 12:50:47 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -8927,7 +8927,7 @@ def ourPartners155():
             if fileCount!=0:
                 print(fileCount)
                 for ll in range(fileCount):
-                    print(l1,"Timmmmmmmmmmmmmmmmmesssssss")
+                    print(ll,"Timmmmmmmmmmmmmmmmmesssssss")
                     file= request.files.get('postImage_'+str(ll+1)+'')        
                     filename = file.filename or ''                 
                     filename = filename.replace("'","") 
