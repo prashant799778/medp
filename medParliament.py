e0bc8249a92ea7463e736816c8e0fc110d4a6a19
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-23 12:27:12 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -2697,9 +2697,9 @@ def myInbox():
                     if data1['result']!="":
                         for l in data1['result']:
                             data55.append(l)
-                    data55=sorted(data55, key = lambda i: i['DateCreate'])
-                    data55=data55[::-1]
-                            
+                data55=sorted(data55, key = lambda i: i['DateCreate'])
+                data55=data55[::-1]
+                        
 
                             
                             
