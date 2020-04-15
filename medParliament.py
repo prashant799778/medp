f6e5ac223a17b814f88ebe7b91005b93557cce84
prashant <prashantgoyal.fourbrick@gmail.com>
2020-04-15 13:18:23 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -5343,16 +5343,17 @@ def allMarketingInsightThread1():
               
                 column1="pm.id,um.userName,um.email,pm.Status,pm.commentDescription,(pm.userId)commentedBy,pm.userTypeId, date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
                 WhereCondition="  and pm.userId=um.userId and pm.marketingInsightId='" + str(marketingInsightId) + "'" 
-                orderby=" id "
+                orderby="  "
 
                 data1 = databasefile.SelectQueryOrderbyAsc("marketingInsightComment as pm,userMaster as um",column1,WhereCondition,"",orderby,startlimit,endlimit)
+                print(data1,"++++++++++++++++++=")
                 WhereCondition=" and n.id='"+str(marketingInsightId)+"'"
 
                 data2=databasefile.SelectQuery4("marketingInsightComment as pm,userMaster as um",column1,WhereCondition)
                 column = " n.id,n.Status,n.newsTitle,n.userTypeId,n.summary,n.newsDesc, date_format(CONVERT_TZ(n.DateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate, concat('"+ ConstantData.GetBaseURL() + "',n.imagePath)imagePath ,um.userName "
                 data = databasefile.SelectQuery1("marketingInsights n,userMaster um",column,WhereCondition)
                 print(data,"111111111111111111")
-            print(data,"22222222222222")
+            
 
 
 
