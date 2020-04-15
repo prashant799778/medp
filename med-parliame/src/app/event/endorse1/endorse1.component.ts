6f5298fe1a1820d3a05620bc74326ef0734a0b6d
vijay.pal <vijay.pal@fourbrick.com>
2020-04-15 13:51:57 +0530

    add pagingation

--- med-parliame/src/app/event/endorse1/endorse1.component.ts
+++ med-parliame/src/app/event/endorse1/endorse1.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserServiceService } from 'src/app/services/user-service.service';
 import { AppSettings } from 'src/app/utils/constant';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-endorse1',
@@ -13,23 +14,81 @@ export class Endorse1Component implements OnInit {
   allnews=[];
 
 
-  constructor(public route: ActivatedRoute, private apiService: UserServiceService,) { }
+  pageSize: number = 10;
+  totalRecords: number;
+  paginationDisplay: boolean;
+  frmShowNews: FormGroup;
+
+
+  constructor(public fb: FormBuilder,public route: ActivatedRoute, private apiService: UserServiceService,) { 
+    this.frmShowNews = this.fb.group({
+      startlimit: [0],
+      endlimit: [10],
+      Id: ['']
+     
+    });
+  }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       console.log(params);
       this.newsId = params['NewsId'];
       console.log(this.newsId);
-      let data ={
-        'Id': this.newsId
-      }
-      this.apiService.dataPostApi(data, AppSettings.likesgetParliamentEventlikes).then(resp=>{
-        console.log(resp)
-        if(resp && resp['status']=='true'){
-          this.allnews = resp['result']
-        }
-      })
+      this.getNews()
+     
     });
+  }
+
+  pageChanged(event){
+    console.log(event)
+    // this.pageSize = event;
+    // let totalpagess = (this.totalRecords / 2)
+    let endlimit = this.pageSize;
+    let startlimit = (this.pageSize * event) - this.pageSize;
+    if(endlimit > this.totalRecords){
+    endlimit = this.totalRecords;
+    
+    this.frmShowNews.get('endlimit').setValue(endlimit)
+    }else{
+    this.frmShowNews.get('endlimit').setValue(this.pageSize)
+    }
+    
+    this.frmShowNews.get('startlimit').setValue(startlimit)
+    // this.frmShowNews.get('CategoryId').setValue(this.CategoryId)
+    let data = this.frmShowNews.getRawValue();
+    
+    this.apiService.dataPostApi(data,AppSettings.likesgetParliamentEventlikes).then((data: any[]) => {
+      this.totalRecords = data['totalCount']
+      console.log(this.totalRecords)
+      if(this.totalRecords > this.pageSize){
+        console.log("inside if",this.totalRecords)
+        this.paginationDisplay = true;
+        }else{
+          console.log("inside else",this.totalRecords)
+        this.paginationDisplay = false;
+        }
+      this.allnews = data['result'];
+      
+    });
+    }
+
+  getNews() {
+    this.frmShowNews.get('Id').setValue(this.newsId)
+    let data = this.frmShowNews.getRawValue()
+      this.apiService.dataPostApi(data,AppSettings.likesgetParliamentEventlikes).then((data: any[]) => {
+        this.totalRecords = data['totalCount']
+
+        if(this.totalRecords > this.pageSize){
+          console.log("inside if",this.totalRecords)
+          this.paginationDisplay = true;
+          }else{
+            console.log("inside else",this.totalRecords)
+          this.paginationDisplay = false;
+          }
+        this.allnews = data['result'];
+        console.log(this.allnews)
+      });
+     
   }
 
 }
