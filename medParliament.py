3f763e21580a69184020cd7b85cce3d122a07568
prashant <prashantgoyal.fourbrick@gmail.com>
2020-04-17 18:00:46 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -10072,9 +10072,10 @@ def superAdminNotificationCount1():
             column="pm.postDescription,pm.postId,pm.userId,pm.status,pm.id as Id,pm.postImage,pm.postTitle,pm.postImagePath,pm.userTypeId as userTypeId,date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
             WhereCondition= "and pm.superadminStatus='1'"
             data22 = databasefile.SelectQuery4("userPost as pm",column,WhereCondition)
+            a=[]
             
             if data22['status'] !='false':
-                a=[]
+               
                 for i in data22['result']:
 
                     postId=i['postId']
