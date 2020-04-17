f5dbf7cedc07caaed54d414afbd95e51071d18dc
prashant <prashantgoyal.fourbrick@gmail.com>
2020-04-17 17:09:31 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -10067,7 +10067,7 @@ def superAdminNotificationCount1():
             for i in data['result']:
                 postId=i['postId']
                 print("111111111111111")  
-                column= "pm.commentDescription as postDescription,um.userName,pm.postId,pm.userId,pm.status,pm.id as Id,up.postTitle,pm.userTypeId as userTypeId,date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
+                column= "pm.commentDescription as postDescription,um.userName,pm.postId,up.userId,pm.status,pm.id as Id,up.postTitle,pm.userTypeId as userTypeId,date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
                 whereCondition= " and pm.approvedUserId=um.userId and pm.superstatus='0'  and pm.postId=up.postId and pm.postId= '"+str(postId)+"'"
                 data2=databasefile.SelectQuery4('approvedBy as pm,userPost as up,userMaster as um',column,whereCondition)
                 print(data2,"@@@@@@@@$%76666666")
