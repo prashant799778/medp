321208daa23cfc0137a626933e04bb153ec5b055
prashant <prashantgoyal.fourbrick@gmail.com>
2020-04-18 18:18:55 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -10174,7 +10174,7 @@ def adminNotificationCount1():
                     print("111111111111111")
 
                     column= "pm.commentDescription as postDescription,um.userName,pm.postId,up.userId,pm.status,pm.id as Id,up.postTitle,pm.userTypeId as userTypeId,date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
-                    whereCondition= " and pm.approvedUserId=um.userId and  pm.adminstatus='0'  and pm.postId=up.postId and pm.postId= '"+str(i['postId'])+"'"
+                    whereCondition= " and pm.approvedUserId=um.userId and pm.userTypeId='" + str(userTypeId) + "' and  pm.adminstatus='0'  and pm.postId=up.postId and pm.postId= '"+str(i['postId'])+"'"
                     data2=databasefile.SelectQuery4('approvedBy as pm,userPost as up,userMaster as um',column,whereCondition)
                     print(data2['result'],"@@@@@@@@$%9999999999999999999999999999999999976666666")
 
