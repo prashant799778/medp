9d00445e63722d452ec851e64ee072ee8e9c827e
prashant <prashantgoyal.fourbrick@gmail.com>
2020-04-18 17:47:39 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -10171,27 +10171,27 @@ def adminNotificationCount1():
                     print(i['postId'],'wwwwwwwwwwww9999999999999999999999999999999999999')
                     print("111111111111111")
 
-                    column= "pm.commentDescription as postDescription,um.userName,pm.postId,up.userId,pm.status,pm.id as Id,up.postTitle,pm.userTypeId as userTypeId,date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
-                    whereCondition= " and pm.approvedUserId=um.userId and pm.adminstatus='0'  and pm.postId=up.postId and pm.postId= '"+str(i['postId'])+"'"
-                    data2=databasefile.SelectQuery4('approvedBy as pm,userPost as up,userMaster as um',column,whereCondition)
-                    print(data2['result'],"@@@@@@@@$%9999999999999999999999999999999999976666666")
+                    # column= "pm.commentDescription as postDescription,um.userName,pm.postId,up.userId,pm.status,pm.id as Id,up.postTitle,pm.userTypeId as userTypeId,date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
+                    # whereCondition= " and pm.approvedUserId=um.userId and pm.adminstatus='0'  and pm.postId=up.postId and pm.postId= '"+str(i['postId'])+"'"
+                    # data2=databasefile.SelectQuery4('approvedBy as pm,userPost as up,userMaster as um',column,whereCondition)
+                    # print(data2['result'],"@@@@@@@@$%9999999999999999999999999999999999976666666")
 
                     
-                    if data2['result']!= "":
-                        print('sggggggggggggggggggggggg',data2['result'])
-                        for m in data2['result']:
-                            b.append(m)
+                    # if data2['result']!= "":
+                    #     print('sggggggggggggggggggggggg',data2['result'])
+                    #     for m in data2['result']:
+                    #         b.append(m)
                         
                         
                         
                         
-                    else:
-                        print("ssssssssss")
-                        data2['result']=b
+                    # else:
+                    #     print("ssssssssss")
+                    #     data2['result']=b
 
-                    Data = {"status":"true","message":"","result":"","totalcount":len(b)}
-                    print(Data,"@@@@@@@@@@@@@@@@@@")
-                    return Data
+                    # Data = {"status":"true","message":"","result":"","totalcount":len(b)}
+                    # print(Data,"@@@@@@@@@@@@@@@@@@")
+                    # return Data
 
                
             else:
