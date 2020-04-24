2447df7f927f9ce07f22bc7015f4b1ea24ea1140
prashant <prashantgoyal.fourbrick@gmail.com>
2020-04-24 23:32:54 +0530

    Update medParliament.py

--- medParliament.py
+++ medParliament.py
@@ -9984,7 +9984,7 @@ def superAdminNotification():
                         print("111111111111111")
 
                         column= "pm.commentDescription as postDescription,um.userName,pm.postId,up.userId,pm.status,pm.id as Id,up.postTitle,pm.userTypeId as userTypeId,date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
-                        whereCondition= " and pm.approvedUserId=um.userId  and  pm.superstatus='0'  and pm.postId=up.postId "
+                        whereCondition= " and pm.approvedUserId=um.userId  and  pm.superstatus='0'  and pm.postId=up.postId and pm.postId= '"+str(i['postId'])+"'"
                         data2=databasefile.SelectQuery4('approvedBy as pm,userPost as up,userMaster as um',column,whereCondition)
                         print(data2['result'],"@@@@@@@@$%9999999999999999999999999999999999976666666")
 
@@ -10059,7 +10059,7 @@ def superAdminNotificationCount():
                     print("111111111111111")
 
                     column= "pm.commentDescription as postDescription,um.userName,pm.postId,up.userId,pm.status,pm.id as Id,up.postTitle,pm.userTypeId as userTypeId,date_format(CONVERT_TZ(pm.dateCreate,'+00:00','+05:30'),'%Y-%m-%d %H:%i:%s')DateCreate"
-                    whereCondition= " and pm.approvedUserId=um.userId  and  pm.superstatus='0'  and pm.postId=up.postId "
+                    whereCondition= " and pm.approvedUserId=um.userId  and  pm.superstatus='0'  and pm.postId=up.postId and pm.postId= '"+str(i['postId'])+"'"
                     data2=databasefile.SelectQuery4('approvedBy as pm,userPost as up,userMaster as um',column,whereCondition)
                     print(data2['result'],"@@@@@@@@$%9999999999999999999999999999999999976666666")
 
