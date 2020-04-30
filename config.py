7aa908916d82c00398ec14621466087e6c699e0e
Basant <basant@fourbrick.com>
2020-05-01 01:55:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 def Connection():
     connection = pymysql.connect(host='localhost',
                                 user='root',
-                                password='OHAvgawzUw##32',
+                                password='Medp$@873',
                                 db='medParliament',
                                 charset='utf8mb4',
                                 cursorclass=pymysql.cursors.DictCursor)
